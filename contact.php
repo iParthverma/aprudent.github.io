e190e222729dc8a1667fb2ce5e4605135f081483
PARTH VERMA <66512330+iParthverma@users.noreply.github.com>
2021-05-12 14:47:38 +0530

    updated php file

--- contact.php
+++ contact.php
@@ -10,8 +10,6 @@ if (empty($name) || empty($email) || empty($message))
 else
 {
     mail("pvverma703@gmail.com", "Aprudent Message", $message, "from: $name<$email>");
-    echo "<script type= 'text/javascript'>alert('Your message sent successfully');
-    window.history.log(-1);
-    </script>";
+    echo "<script type= 'text/javascript'>alert('Your message sent successfully')</script>";
 }
 ?>
